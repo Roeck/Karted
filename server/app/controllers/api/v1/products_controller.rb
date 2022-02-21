3ae94d82ac8f05dedbb2c61250a9b00d8a054f18
Roeck <rons9n@gmail.com>
2022-02-21 16:47:24 -0500

    Product scaffold

---
+++ server/app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,47 @@
+class Api::V1::ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :update, :destroy]
+
+  def index
+    @products = Product.all
+
+    render json: @products
+  end
+
+  def show
+    @product.update_product
+    render json: @product
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+  end
+
+  private
+  
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :category, :description, :image, :price, :brand, :rating, :numReviews, :countInStock)
+    end
+
+end
