49fe3f6eaa3a8475450bd5bd3fd762036b356c96
Roeck <rons9n@gmail.com>
2022-02-21 17:44:45 -0500

    ProductSSSS component

---
+++ client/src/components/Products.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Fade from 'react-reveal';
+import Product from './Product';
+
+const Products = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Fade bottom cascade>
+          <ul className='products'>
+              <li key={product.id}>
+                <Product product={product} />
+              </li>
+          </ul>
+      </Fade>
+    </div>
+  );
+};
+
+export default Products;
