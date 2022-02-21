d1abdba2da45ee72ee04775023e06948e75a5a75
Roeck <rons9n@gmail.com>
2022-02-21 18:17:31 -0500

    Image assets, seed file

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+prod1 = Product.create(name: 'Dell Desktop', category: 'Desktop', image: '/images/desk1.png', 
+description: 'Overall great performance', price: 800.00, brand: 'Dell', countInStock: 10)
+
+prod2 = Product.create(name: 'Acer Desktop', category: 'Desktop', image: '/images/desk2.png', 
+description: 'Among the bests on its category', price: 700.00, brand: 'Acer', countInStock: 1)
+
+prod3 = Product.create(name: 'Apple Desktop', category: 'Desktop', image: '/images/desk3.png', 
+description: 'Overall great performance', price: 1600.00, brand: 'Apple', countInStock: 11)
+
+prod4 = Product.create(name: 'Dell Laptop', category: 'Laptop', image: '/images/lap1.png', 
+description: 'Extremely versatile', price: 800.00, brand: 'Dell', countInStock: 10)
+
+prod5 = Product.create(name: 'Acer Laptop', category: 'Laptop', image: '/images/lap2.png', 
+description: 'Best budget laptop', price: 400.00, brand: 'Acer', countInStock: 2)
+
+prod6 = Product.create(name: 'Apple Laptop', category: 'Laptop', image: '/images/lap3.png', 
+description: 'All-around laptop', price: 1200.00, brand: 'Apple', countInStock: 5)
