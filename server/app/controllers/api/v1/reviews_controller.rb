070c1d6866964d12b021ba6c418c8a8d8ab7d46e
Roeck <rons9n@gmail.com>
2022-02-21 17:10:02 -0500

    Review scaffold

---
+++ server/app/controllers/api/v1/reviews_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::ReviewsController < ApplicationController
+  before_action :set_product, except: [:destroy]
+
+  def index
+    render json: @product.reviews
+  end
+
+  def show
+    @review = Review.find(params[:id])
+    render json: @review
+  end
+
+  def create
+    @review = @product.reviews.new(review_params)
+    @review.save
+    render json: @product
+  end
+
+  private
+
+    def set_product
+      @product = Product.find(params[:product_id])
+    end
+
+    def review_params
+      params.require(:review).permit(:product_id, :rating, :comment)
+    end
+
+end
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_21_214532) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_21_220912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_21_214532) do
     t.float "rating", default: 0.0
     t.integer "countInStock"
     t.integer "numReviews", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "product_id"
+    t.integer "rating"
+    t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
