950c8ae42c5765590e99bdd49766339a3ad7bbfa
Roeck <rons9n@gmail.com>
2022-02-21 18:37:24 -0500

    Initial styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
+import React from 'react';
+import {BrowserRouter,Route,Link} from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
+
 function App() {
   return (
-    <div className="App">
-      <h1>KARTED</h1>
-    </div>
+    <BrowserRouter>
+      <div className='grid-container'>
+        <header className='header'>
+          <div className='brand'>
+            <Link to='/'>KARTED</Link>
+          </div>
+          <div className='header-links'>
+            <a href='cart.html'>Cart</a>
+          </div>
+        </header>
+        <main className='main'>
+          <div className='content'>
+          </div>
+        </main>
+        <footer className='footer'>Copyright &#169; KARTED</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 prod1 = Product.create(name: 'Dell Desktop', category: 'Desktop', image: '/images/desk1.png', 
 description: 'Overall great performance', price: 800.00, brand: 'Dell', countInStock: 10)
 
